90dca048af6bb37fb32ef6412cbd454aa9cb54ea
valdeir <valdeir_014@hotmail.com>
2022-12-06 22:25:46 -0300

    Projeto aula base concluido

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+let corTela = "#b2a103";
+let corTelaDesenho ="#343300";
+let corTelaFood = "#606404";
 snake[0]={
     x:8 * box,
     y:8 * box
@@ -12,27 +15,27 @@ let direction = "right";
 
 //gera uma posicao aleatoria para a comida
 let food = {
-    x:Math.floor(Math.random() * 15 + 1) * box,
-    y:Math. floor(Math.random() * 15 + 1) * box
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
 }
 
 //cria a tela de ação
 function criarBG(){
-    context.fillStyle = "lightgreen";
+    context.fillStyle = corTela;
     context.fillRect(0, 0, 16 * box, 16 * box); 
 }
 
 //cria a cobrinha na tela de ação
 function criarCobrinha(){
-    for(i=0; i < snake.length;i ++){
-        context.fillStyle = "green";
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = corTelaDesenho;
         //desenha a cobrinha na tela com o tamanho do pixel
         // o valor 8 de x e y para iniciar no meio da tela de 32
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 function drawFood(){
-    context.fillStyle = "red";
+    context.fillStyle = corTelaFood;
     context.fillRect (food.x, food.y, box, box);
 }
 
@@ -47,12 +50,20 @@ function updateEntrada(event){
 }
 
 function inicarJogo(){
+
     //quando atingir o final da tela de ação passar passa o outro lado
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0  && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0  && direction == "up") snake[0].y = 16 * box;
 
+    //verifica se ha contato com o co proprio corpo
+    for(i=1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
     
     criarBG();
     criarCobrinha(); 
@@ -67,8 +78,14 @@ function inicarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
+    if(snakeX !=food.x || snakeY != food.y){
     //remove o ultimo elemento da cobrinha
     snake.pop();
+    }
+    else{
+        food.x =Math.floor(Math.random() * 15 + 1) * box,
+        food.y = Math. floor(Math.random() * 15 + 1) * box;
+    }   
 
     //nova cabeca para a cobrinha
     let newHead = {
